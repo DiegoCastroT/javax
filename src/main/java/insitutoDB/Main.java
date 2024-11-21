41d84e978f28e0a8e4260880a64fd84f1df3711b
DiegoCastroT <diegodct6@gmail.com>
2024-11-21 20:50:43 +0100

    Added new exercise

--- src/main/java/insitutoDB/Main.java
+++ src/main/java/insitutoDB/Main.java
@@ -1,10 +1,45 @@
 package insitutoDB;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+
 import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 public class Main {
 
     public static void main(String[] args) {
+        Properties props = new Properties();
+        MysqlDataSource ds = null;
+        FileInputStream fis = null;
+
+        try{
+            fis = new FileInputStream("db.propierties");
+            props.load(fis);
+
+            ds = new MysqlDataSource();
+
+            ds.setURL(props.getProperty("URL"));
+            ds.setUser(props.getProperty("USUARIO"));
+            ds.setPassword(props.getProperty("PASSWORD"));
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }finally {
+            try {
+                if (fis != null) {
+                    fis.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
 
     }
 }
